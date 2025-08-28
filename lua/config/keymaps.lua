d5cae1fc8f770d84064b9d66f9ede4a9dc192773
Matheus G. Dias <matheusgiga123@gmail.com>
2025-08-28 16:03:25 -0300

    keymaps lsp diagnostic

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,9 +7,10 @@ setmap('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
 setmap('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
 setmap('n', '<leader>s', telescope.lsp_document_symbols, { desc = 'Telescope show document symbols' })
 
-setmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+setmap('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>', { desc = 'LSP Workspace Diagnostics' })
+setmap("n", "<leader>df", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
 
-setmap("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+setmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
 setmap('n', '<leader>q', '<CMD>:quit<CR>', { desc = 'Quit' })
 setmap('n', '<leader>\\', ':vsplit<CR>', { noremap = true, silent = true })
