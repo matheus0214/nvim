9f221d5aa75460892ad1f1814bcbb68b2ffccfda
Matheus G. Dias <matheusgiga123@gmail.com>
2025-09-02 20:01:15 -0300

    config tab to show only the filename

--- init.lua
+++ init.lua
@@ -6,3 +6,24 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
   command = "checktime",
 })
 
+
+vim.o.tabline = "%!v:lua.MyTabLine()"
+
+function _G.MyTabLine()
+  local s = ""
+  for i = 1, vim.fn.tabpagenr("$") do
+    local buflist = vim.fn.tabpagebuflist(i)
+    local winnr = vim.fn.tabpagewinnr(i)
+    local bufname = vim.fn.bufname(buflist[winnr])
+    local filename = vim.fn.fnamemodify(bufname, ":t") -- get only filename
+
+    if i == vim.fn.tabpagenr() then
+      -- current tab
+      s = s .. "%#TabLineSel#" .. " " .. filename .. " "
+    else
+      s = s .. "%#TabLine#" .. " " .. filename .. " "
+    end
+  end
+  s = s .. "%#TabLineFill#"
+  return s
+end
